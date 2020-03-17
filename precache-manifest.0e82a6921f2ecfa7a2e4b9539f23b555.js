90fb3f452fd4ba205394b3f44e870a2e40a18ac7
RagnnnaCode <filigreepvp@mail.ru>
2020-03-17 11:22:04 +0300

    Updates

---
+++ precache-manifest.0e82a6921f2ecfa7a2e4b9539f23b555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9703b10d9ecf2fba07d9acd4df628fc",
+    "url": "/wstats/index.html"
+  },
+  {
+    "revision": "60cf86db95f3062c1087",
+    "url": "/wstats/static/css/main.e91c7b95.chunk.css"
+  },
+  {
+    "revision": "6bf7eee5153a113ad92a",
+    "url": "/wstats/static/js/2.1977c2fc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/wstats/static/js/2.1977c2fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cf86db95f3062c1087",
+    "url": "/wstats/static/js/main.a44ae4d0.chunk.js"
+  },
+  {
+    "revision": "e34a279f23fc62f9c29f",
+    "url": "/wstats/static/js/runtime-main.9f7a925b.js"
+  }
+]);
